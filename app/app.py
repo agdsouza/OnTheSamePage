95e1fc61231f184e52e7391380b3ea679894451d
Adriana D'Souza <adsouza@Adrianas-Air.home>
2018-07-02 00:36:49 -0400

    fixed x-axis problems and added css

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 import dash
-from dash.dependencies import Output, Event
+import dash.dependencies
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly
@@ -7,45 +7,126 @@ import random
 import plotly.graph_objs as go
 from collections import deque
 import cassandra_data
-import datetime
+from datetime import datetime
 import pandas as pd
+import flask
+import numpy as np
 
-host = '52.73.46.190'
-keyspace = 'site_log'
+HOST = '52.73.46.190'
+KEYSPACE = 'site_log'
+WEBSITE = 'http://myfancysite.com/'
 
-session = cassandra_data.start_connection(host, keyspace)
-prep_query = cassandra_data.prepare_page_visits_query(session)
-page = 'http://myfancysite.com/page760'
+session = cassandra_data.start_connection(HOST, KEYSPACE)
+prep_page_query = cassandra_data.prepare_page_visits_query(session)
+prep_avg_query = cassandra_data.prepare_avg_visits_query(session)
 
-app = dash.Dash(__name__)
+PAGES = ['page' + str(n) for n in range(1, 1001)]
+
+POINTS_MIN = 10
+POINTS_MAX = 100
+
+server = flask.Flask(__name__)
+app = dash.Dash(__name__, server=server)
+
 app.layout = html.Div(
     [
-        dcc.Graph(id='live-graph', animate=True),
+        html.Div([
+        html.H2('On the Same Page',
+                style={'float': 'left',
+                       }),
+        ]),
+    dcc.Input(id='num_points', value='', type='text'),
+        dcc.Dropdown(id='page_name',
+             options=[{'label': p, 'value': p} for p in PAGES],
+             multi=True
+         ),
+    html.Div(children=html.Div(id='graphs'), className='row'),
         dcc.Interval(
             id='graph-update',
             interval=1*1000
         ),
-    ]
+    ], className="container",style={'width':'98%','margin-left':10,'margin-right':10,'max-width':50000}
 )
 
-@app.callback(Output('live-graph', 'figure'),
-              events=[Event('graph-update', 'interval')])
-def update_graph_scatter():
-    df = cassandra_data.get_page_visit_count(page, prep_query, session)
-    X = df.interval_time.values[-20:]
-    Y = df.visits.values[-20:]
+@app.callback(
+    dash.dependencies.Output('graphs', 'children'),
+    [dash.dependencies.Input('num_points', 'value'),
+    dash.dependencies.Input('page_name', 'value')],
+    events=[dash.dependencies.Event('graph-update', 'interval')])
+def update_graph(num_points, page_name):
+    graphs = []
 
-    data = plotly.graph_objs.Scatter(
-            x=list(X),
-            y=list(Y),
-            name='Scatter',
-            mode= 'lines+markers'
-            )
+    # configure input for number of points to be displayed on the UI
+    try: 
+        points = int(num_points)
+        if points < POINTS_MIN:
+            points = POINTS_MIN
+        elif points > POINTS_MAX:
+            points = POINTS_MAX
+    except:
+        points = POINTS_MIN
 
-    return {'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)]),
-                                                yaxis=dict(range=[min(Y),max(Y)]),)}
+    # alter display of graphs depending on how many graphs are chosen to be shown
+    if len(page_name)>2:
+        class_choice = 'col s12 m6 l4'
+    elif len(page_name) == 2:
+        class_choice = 'col s12 m6 l6'
+    else:
+        class_choice = 'col s12'
 
+    for p in page_name:
+        # dataframe for the page views every 5 seconds
+        df1 = cassandra_data.get_page_visit_count(WEBSITE + p, prep_page_query, session)
+        df1['interval_time'] = df1['interval_time'].map(lambda x: \
+            str(x - np.timedelta64(4,'h'))[:10] + " " + str(x - np.timedelta64(4,'h'))[11:19])
 
+        X = df1.interval_time.values[:points]
+        Y = df1.visits.values[:points]
+
+        # dataframe for the average page views every 5 seconds
+        df2 = cassandra_data.get_page_averages(WEBSITE + p, prep_avg_query, session)
+        avg_line = df2.average.values[0]
+
+        data = plotly.graph_objs.Scatter(
+                x=list(X),
+                y=list(Y),
+                name='Scatter',
+                mode= 'lines+markers'
+                )
+
+        graphs.append(html.Div(dcc.Graph(
+            id=p,
+            animate=True,
+            figure={'data': [data],'layout' : go.Layout(xaxis=dict(range=[min(X),max(X)], tickformat='%H:%M:%S'),
+                                                yaxis=dict(range=[min(Y),max(Y)]),
+                                                title=p,
+                                                shapes=[
+                                                        {
+                                                            'type': 'line',
+                                                            'xref': 'paper',
+                                                            'x0': 0,
+                                                            'y0': avg_line, # use absolute value or variable here
+                                                            'x1': 1,
+                                                            'y1': avg_line, # ditto
+                                                            'line': {
+                                                                'color': 'rgb(50, 171, 96)',
+                                                                'width': 1,
+                                                                'dash': 'dash',
+                                                            },
+                                                        },
+                                                    ])}), className=class_choice))
+    return graphs
+
+external_css = ["https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css"]
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
+external_js = ['https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js']
+for js in external_css:
+    app.scripts.append_script({'external_url': js})
 
 if __name__ == '__main__':
-    app.run_server(debug=True)
+    app.run_server(host="0.0.0.0", port=80)
+
+
+    
--- app/cassandra_data.py
+++ app/cassandra_data.py
@@ -1,22 +1,38 @@
 from cassandra.cluster import Cluster
+from cassandra.query import dict_factory
+from cassandra import ReadTimeout
 import pandas as pd
 import time
 import datetime
 
-host = '52.73.46.190'
-keyspace = 'site_log'
-page = 'http://myfancysite.com/page760'
-
 def start_connection(host, keyspace):
 	cluster = Cluster([host])
 	session = cluster.connect(keyspace)
+	session.row_factory = dict_factory
 	return session
 
 def prepare_page_visits_query(session):
-	query = "SELECT * FROM page_visit_table WHERE url=? LIMIT 20"
+	query = "SELECT * FROM page_visit_table WHERE url=? LIMIT 100"
 	return session.prepare(query)
 
 def get_page_visit_count(page, prepared_query, session):
-	count = session.execute(prepared_query, [page])
-	df = pd.DataFrame(list(count))
+	count = session.execute_async(prepared_query, [page])
+	try:
+		rows = count.result()
+		df = pd.DataFrame(list(rows))
+	except ReadTimeout:
+	    log.exception("Query timed out:")
+	return df
+
+def prepare_avg_visits_query(session):
+	query = "SELECT * FROM page_averages WHERE url=? AND average>4 LIMIT 1 ALLOW FILTERING"
+	return session.prepare(query)
+
+def get_page_averages(page, prepared_query, session):
+	avg = session.execute_async(prepared_query, [page])
+	try:
+		rows = avg.result()
+		df = pd.DataFrame(list(rows))
+	except ReadTimeout:
+	    log.exception("Query timed out:")
 	return df
