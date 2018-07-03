92ff5f7ac691581f14b2a0775b39e39c4e9e38fa
Adriana D'Souza <adsouza@Adrianas-MacBook-Air.local>
2018-07-03 10:01:16 -0400

    fixed constants and removed comments

--- spark/streaming_counts/src/main/scala/site_data.scala
+++ spark/streaming_counts/src/main/scala/site_data.scala
@@ -9,6 +9,7 @@ import org.apache.spark.sql._
 import org.apache.spark.sql.functions._
 import com.datastax.spark.connector.streaming._
 import com.datastax.spark.connector._
+import com.typesafe.config.{Config, ConfigFactory}
 import java.sql.Timestamp
 import java.text.SimpleDateFormat
 import java.util.{Calendar, Date}
@@ -21,53 +22,48 @@ object site_data {
     val brokers = "ec2-18-204-77-15.compute-1.amazonaws.com:9092"
     val topics = "site_log"
     val topicsSet = topics.split(",").toSet
-    val interval = 5
+    // mini batch interval; constant of 5
+    val Interval = 5
 
-    // Create context with 2 second batch interval
-    val sparkConf = new SparkConf().setAppName("price_data")
-    val ssc = new StreamingContext(sparkConf, Seconds(interval))
+    // Create context with 5 second batch interval
+    val sparkConf = new SparkConf().setAppName("price_data").set("spark.streaming.backpressure.enabled", "false")
+    val ssc = new StreamingContext(sparkConf, Seconds(Interval))
 
     // Create direct kafka stream with brokers and topics
     val kafkaParams = Map[String, String]("metadata.broker.list" -> brokers)
     val messages = KafkaUtils.createDirectStream[String, String, StringDecoder, StringDecoder](ssc, kafkaParams, topicsSet)
     ssc.checkpoint("s3n://sessionizationbucket/site_log_checkpoint/")
 
-    // gets the current time stamp to put in the database
     def getCurrentdateTimeStamp: Timestamp ={
-      val today:java.util.Date = Calendar.getInstance.getTime
-      val timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
-      val now:String = timeFormat.format(today)
-      val re = java.sql.Timestamp.valueOf(now)
-      return re
+        val today:java.util.Date = Calendar.getInstance.getTime
+        val timeFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+        val now:String = timeFormat.format(today)
+        val re = java.sql.Timestamp.valueOf(now)
+        return re
     }
-
-    // start processing the data
+    
+    // Get the lines and show results
     val lines = messages.map(_._2)
     val ticksDF = lines.map( x => {
-                              val tokens = x.split(";")
-                              (tokens(2), tokens(1), tokens(3))}).persist()
+                             val tokens = x.split(";")
+                             (tokens(2), tokens(1), tokens(3))}).persist()
 
-    // write raw data into cassandra for batch processing
+    // generate RDD for raw data
     val ticks_raw = ticksDF.map(record => (record._1, java.sql.Timestamp.valueOf(record._2), record._3))
+
+    // write raw data into cassandra for later
     ticks_raw.saveToCassandra("site_log", "raw_data", SomeColumns("ip", "starttime", "url"))
 
     // get user count every second, then add to page_visits table
     val ticks_interval_page = ticksDF.map(record => ((record._3), 1))
-                                     .reduceByKeyAndWindow((a:Int, b:Int) => a + b, Seconds(interval), Seconds(interval))
-                                     .map(record => (record._1, getCurrentdateTimeStamp, interval, record._2))
-    ticks_interval_page.saveToCassandra("site_log", "page_visit_table", SomeColumns("url", "interval_time", "duration", "visits"))
-
-    // get unique user count every second, then add to page_visits_unique
-    val ticks_interval_unique = ticksDF.window(Seconds(5), Seconds(5))
-                                       .map(record => ((record._1,record._3), 1))
-                                       .reduceByKey( (x, y) => (x) )
-                                       .map(record => ((record._1._2), 1))
-                                       .reduceByKey((a:Int, b:Int) => a + b)
-                                       .map(record => (record._1, getCurrentdateTimeStamp, interval, record._2))
-    ticks_interval_unique.saveToCassandra("site_log", "unique_page_visit_table", SomeColumns("url", "interval_time", "duration", "unique_visits"))
+                                            .reduceByKeyAndWindow((a:Int, b:Int) => a + b, Seconds(Interval), Seconds(Interval))
+                                            .map(record => (record._1, getCurrentdateTimeStamp, record._2))
+    ticks_interval_page.saveToCassandra("site_log", "page_visit_table", SomeColumns("url", "interval_time", "visits"))
 
     // Start the computation
     ssc.start()
     ssc.awaitTermination()
   }
 }
+
+
